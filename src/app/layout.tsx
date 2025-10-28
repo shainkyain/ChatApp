3e10f44fa70a61eee8842beae18bd7d43aa9d98e
Shainky Jain <116502451+shainkyain@users.noreply.github.com>
2025-10-28 10:24:02 +0530

    deployment-changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,15 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-const geistSans = Geist({   
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({   
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
           
 export const metadata: Metadata = {
   title: "ChatApp",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import Header from './Components/Header';
 import Home from './Components/Home';
-import Features from './Components/Features';
-import Footer from './Components/Footer';
 
 export default function Page() {
   return (
