d86cdf3058f9c99f236594e77e07c5b2ccc23ad5
Shainky Jain <116502451+shainkyain@users.noreply.github.com>
2025-10-28 10:17:51 +0530

    ..

--- .next/static/chunks/src_app_ai-chat_page_tsx_3de55072._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_Components_AIChat_tsx_d4a6ba7b._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_3de55072._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_0bae1ead._.js"
-],
-    source: "dynamic"
-});
